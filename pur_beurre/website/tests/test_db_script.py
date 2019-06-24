e88e943f7f4a27427a11e1a45ac56fbed87bd0b7
Othman Andour <andour.otman@gmail.com>
2019-06-24 23:18:10 +0200

    3 tests failed out of 30

--- pur_beurre/website/tests/test_db_script.py
+++ pur_beurre/website/tests/test_db_script.py
@@ -1,6 +1,6 @@
 """ File testing database script """
 
-from website.db_script import api_request
+from website.db_script import *
 import requests
 import json
 import codecs
@@ -10,6 +10,29 @@ from django.test import TestCase
 class TestDbScript(TestCase):
     """Database tests class"""
     
+    def setUp(self):
+        """ Create a test product """
+        
+        self.product = {
+            "product_name" : "nutella",
+            "nutrition_grade_fr" : "e",
+            'nutriments' : {
+                'energy_100g' : '5',
+                'sugars_100g' : '5',
+                'salt_100g' : '5',
+                'fat_100g' : '5',
+                'saturated-fat_100g' : '5',
+                'sodium_100g' : '5',
+                'proteins_100g' : '5',
+                'carbohydrates_100g' : '5',
+                },
+            'categories' : 'Pâte à tartiner',
+            'stores' : 'Carrefour',
+            'url' : 'testurl.com',
+            'image_url' : 'testimageurl.com',
+            'image_small_url' : 'testimagesmallurl.com'
+        } 
+
 #   Trying to mock, but not working
 #   def test_api_request(self, monkeypatch):
 #       """ Testing the OFF API request """
@@ -39,4 +62,31 @@ class TestDbScript(TestCase):
 #
 #       self.assertEquals(api_request(1), results)
 
+    def test_product_name(self):
+        self.assertEquals(product_name(self.product), "nutella")
 
+    def test_has_nutriscore(self):
+        self.assertEquals(has_nutriscore(self.product), "e")
+
+    def test_product_nutriments(self):
+        data = {
+            'energy_100g' : '5',
+            'sugars_100g' : '5',
+            'salt_100g' : '5',
+            'fat_100g' : '5',
+            'saturated-fat_100g' : '5',
+            'sodium_100g' : '5',
+            'proteins_100g' : '5',
+            'carbohydrates_100g' : '5'
+        }
+        self.assertEquals(product_nutriments(self.product), data)
+
+    def test_product_infos(self):
+        data = {
+            'categories' : 'Pâte à tartiner',
+            'stores' : 'Carrefour',
+            'url' : 'testurl.com',
+            'image_url' : 'testimageurl.com',
+            'image_small_url' : 'testimagesmallurl.com'
+        }
+        self.assertEquals(product_infos(self.product), data)
